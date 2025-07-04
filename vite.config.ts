046f2882d6bdef5f376cc34d42fa74b85de0b7a0
rex-light <futurebattery00@gmail.com>
2025-07-04 16:05:20 +0700

    [update] seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import mkcert from 'vite-plugin-mkcert'
- 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
+import { VitePWA, type VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  // includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  workbox: {
+    maximumFileSizeToCacheInBytes: 25 * 1024 * 1024,
+  },
+  manifest: {
+    name: "CrimsonFate",
+    short_name: "crimsonfate",
+    description: "CrimsonFate is a web-based game in Starknet",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-maskable-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+    ],
+    theme_color: "red",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 export default defineConfig({
-  plugins: [react(), mkcert()],
-})
+  plugins: [react(), mkcert(), VitePWA(manifestForPlugIn)],
+  server: {
+    host: true,
+  },
+});
